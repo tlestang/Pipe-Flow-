1ca78473aaabe71e35651a995bfa7518aa9023bc
tlestang <thibault.lestang@ens-lyon.fr>
2015-10-28 12:16:43 +0100

    Correct prototype for streaming and collision routine for body force

--- streamCollCompute.h
+++ streamCollCompute.h
@@ -1,4 +1,4 @@
-void streamingAndCollisionComputeSquareBodyForce(double ***, double ***, double **, double ***, int, int, double, int ,int, int, int, double);
+void streamingAndCollisionComputeSquareBodyForce(double ***, double ***, double **, double ***, int, int, double, double)
 void streamingAndCollisionComputeMacro
 (double***, double***, double**, double***, int, int, double);
 void streamingAndCollisionComputeSquare
